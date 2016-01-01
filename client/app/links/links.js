23b0f4882848e971a91342c0ceb4e454c1334320
anthonychung14 <anthonychung14@gmail.com>
2015-12-31 17:28:13 -0800

    passing all tests. BOOYA

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,7 +1,7 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function($scope, Links) {
-  // Your code here
+  //Why isn't this getting wiped every single time if we couldn't declare an empty array for links?
   $scope.data = {};
 
   Links.getAll().then(function(data) {
