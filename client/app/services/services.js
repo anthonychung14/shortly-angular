97e3d889d7bcf13b12100e5cf76f53238ed0a656
anthonychung14 <anthonychung14@gmail.com>
2015-12-31 17:11:21 -0800

    first commit. links factory working. working on shorten controller next

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,52 +1,82 @@
 angular.module('shortly.services', [])
 
-.factory('Links', function ($http) {
-  // Your code here
-})
-.factory('Auth', function ($http, $location, $window) {
-  // Don't touch this Auth service!!!
-  // it is responsible for authenticating our user
-  // by exchanging the user's username and password
-  // for a JWT from the server
-  // that JWT is then stored in localStorage as 'com.shortly'
-  // after you signin/signup open devtools, click resources,
-  // then localStorage and you'll see your token from the server
-  var signin = function (user) {
-    return $http({
-      method: 'POST',
-      url: '/api/users/signin',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
-  };
+.factory('Links', function($http) {
 
-  var signup = function (user) {
-    return $http({
-      method: 'POST',
-      url: '/api/users/signup',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
-  };
+    // Your code here
 
-  var isAuth = function () {
-    return !!$window.localStorage.getItem('com.shortly');
-  };
+    var getAll = function(links) {
+      return $http({
+          method: 'GET',
+          url: '/api/links',
+          data: links
+        })
+        .then(function(resp) {
+          return resp.data;
+        });
 
-  var signout = function () {
-    $window.localStorage.removeItem('com.shortly');
-    $location.path('/signin');
-  };
+    };
+
+    var addOne = function(link) {
+      return $http({
+          method: 'POST',
+          url: '/api/links',
+          data: link
+        })
+        .then(function(resp) {
+          return resp;
+        });
+    };
+
+    return {
+      getAll: getAll,
+      addOne: addOne
+    };
+
+  })
+  .factory('Auth', function($http, $location, $window) {
+    // Don't touch this Auth service!!!
+    // it is responsible for authenticating our user
+    // by exchanging the user's username and password
+    // for a JWT from the server
+    // that JWT is then stored in localStorage as 'com.shortly'
+    // after you signin/signup open devtools, click resources,
+    // then localStorage and you'll see your token from the server
+    var signin = function(user) {
+      return $http({
+          method: 'POST',
+          url: '/api/users/signin',
+          data: user
+        })
+        .then(function(resp) {
+          return resp.data.token;
+        });
+    };
+
+    var signup = function(user) {
+      return $http({
+          method: 'POST',
+          url: '/api/users/signup',
+          data: user
+        })
+        .then(function(resp) {
+          return resp.data.token;
+        });
+    };
+
+    var isAuth = function() {
+      return !!$window.localStorage.getItem('com.shortly');
+    };
+
+    var signout = function() {
+      $window.localStorage.removeItem('com.shortly');
+      $location.path('/signin');
+    };
 
 
-  return {
-    signin: signin,
-    signup: signup,
-    isAuth: isAuth,
-    signout: signout
-  };
-});
+    return {
+      signin: signin,
+      signup: signup,
+      isAuth: isAuth,
+      signout: signout
+    };
+  });
